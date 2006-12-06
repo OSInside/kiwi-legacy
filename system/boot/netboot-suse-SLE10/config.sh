1104f0419893f1e143be88d5ab4b196955147219
Marcus Schaefer <ms@novell.com>
2006-12-06 15:52:13 +0000

    - added SLE10 netboot descriptions usable for sles and sled   both distributions doesn't provide smart and atftp which are   needed for kiwi and PXE boot
    git-svn-id: https://svn.berlios.de/svnroot/repos/kiwi/kiwi-head@119 92bc7f66-5d17-0410-ac0d-e081219a1bb3

---
+++ system/boot/netboot-suse-SLE10/config.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Configure image: [netboot-suse-SLE10]..."
+test -f /.profile && . /.profile
+
+#==========================================
+# Install default kernel
+#------------------------------------------
+smart install kernel-default -y >/dev/null 2>&1
+
+#==========================================
+# Copy linuxrc to init
+#------------------------------------------
+cp linuxrc init
+
+exit 0
