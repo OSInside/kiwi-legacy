81dd3860ecd99e96f1e529cd58c75337849e189d
Marcus Schaefer <ms@novell.com>
2007-01-19 13:59:14 +0000

    - added bsmd init script for buildservice image. This runlevel   script is used to automatically setup a md0 raid for /abuild
    git-svn-id: https://svn.berlios.de/svnroot/repos/kiwi/kiwi-head@135 92bc7f66-5d17-0410-ac0d-e081219a1bb3

--- system/buildhost-suse-10.1/config.sh
+++ system/buildhost-suse-10.1/config.sh
@@ -10,7 +10,7 @@ for i in \
 	acpid dbus boot.loadmodules boot.localfs boot.xen random \
 	resmgr boot.cleanup boot.localnet haldaemon network syslog \
 	portmap kbd sshd boot.clock nscd cron boot.rootfsck \
-	boot.device-mapper boot.lvm xend ntp bsworker
+	boot.device-mapper boot.lvm xend ntp bsworker bsmd
 do
 	/sbin/insserv /etc/init.d/$i
 done
