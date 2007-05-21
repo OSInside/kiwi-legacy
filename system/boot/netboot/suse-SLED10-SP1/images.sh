20a4aeabe394c134b24c0c9f5bb8f0521e1b68df
Marcus Schaefer <ms@novell.com>
2007-05-21 09:28:59 +0000

    - added required netboot versions for SLES and SLED   GA and SP1 (#270881)
    git-svn-id: https://svn.berlios.de/svnroot/repos/kiwi/kiwi-head@349 92bc7f66-5d17-0410-ac0d-e081219a1bb3

---
+++ system/boot/netboot/suse-SLED10-SP1/images.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+test -f /.profile && . /.profile
+
+echo "Configure image: [$name]..."
+
+#==========================================
+# remove unneeded packages
+#------------------------------------------
+for i in \
+	perl glibc-locale man info smart python \
+	python-xml python-elementtree perl-gettext \
+	perl-Bootloader pam-modules gawk gnome-filesystem \
+	openslp rpm-python suse-build-key permissions \
+	fillup pam expat suse-release libxml2 openldap2-client \
+	logrotate diffutils cpio bzip2 insserv ash gdbm rpm
+do
+	rpm -e $i --nodeps
+done
+
+#==========================================
+# remove unneeded files
+#------------------------------------------
+rm -rf `find -type d | grep .svn`
+rm -rf /usr/share/info
+rm -rf /usr/share/man
+rm -rf /usr/share/cracklib
+rm -rf /usr/lib/python*
+rm -rf /usr/lib/perl*
+rm -rf /usr/share/locale
+rm -rf /usr/share/doc/packages
+rm -rf /var/lib/rpm
+rm -rf /usr/lib/rpm
+rm -rf /var/lib/smart
+rm -rf /boot/* /opt/*
+
+exit 0
