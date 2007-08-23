39b8014ae1918c9005acbb43d287d0bf95f0695b
Marcus Schaefer <ms@novell.com>
2007-08-23 13:42:36 +0000

    - fixed sles10 isoboot images
    git-svn-id: https://svn.berlios.de/svnroot/repos/kiwi/kiwi-head@546 92bc7f66-5d17-0410-ac0d-e081219a1bb3

--- system/boot/isoboot/suse-SLES10-SP1/images.sh
+++ system/boot/isoboot/suse-SLES10-SP1/images.sh
@@ -18,7 +18,7 @@ for i in \
 	perl-Bootloader pam-modules gawk gnome-filesystem \
 	openslp rpm-python suse-build-key permissions \
 	fillup pam expat suse-release libxml2 openldap2-client \
-	logrotate diffutils cpio bzip2 insserv ash gdbm rpm \
+	logrotate diffutils cpio insserv ash gdbm \
 	syslinux gfxboot make memtest86 bootsplash-theme-SuSE-SLES
 do
 	rpm -e $i --nodeps
