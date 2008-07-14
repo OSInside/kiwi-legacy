b937f6e2b8706b05c97923e8a711b1f5a3d247fe
Marcus Schaefer <ms@novell.com>
2008-07-14 09:56:13 +0000

    - check for DHCPSID value too (bnc #408668)
    git-svn-id: https://svn.berlios.de/svnroot/repos/kiwi/kiwi-head@1455 92bc7f66-5d17-0410-ac0d-e081219a1bb3

--- kiwi.pl
+++ kiwi.pl
@@ -43,7 +43,7 @@ use KIWITest;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "2.57";
+our $Version       = "2.58";
 our $openSUSE      = "http://download.opensuse.org/repositories/";
 our $ConfigFile    = "$ENV{'HOME'}/.kiwirc";
 our $ConfigName    = "config.xml";
