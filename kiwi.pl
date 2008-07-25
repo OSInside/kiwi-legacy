351418020418e89a1a993a8d92a8dde25aa12990
Marcus Schaefer <ms@novell.com>
2008-07-25 13:20:16 +0000

    - v2.61 - added Amazon EC2 chapter in kiwi cookbook - fixed reading md5 file in isoboot suse-linuxrc (bnc #411673)
    git-svn-id: https://svn.berlios.de/svnroot/repos/kiwi/kiwi-head@1491 92bc7f66-5d17-0410-ac0d-e081219a1bb3

--- kiwi.pl
+++ kiwi.pl
@@ -43,7 +43,7 @@ use KIWITest;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "2.60";
+our $Version       = "2.61";
 our $openSUSE      = "http://download.opensuse.org/repositories/";
 our $ConfigFile    = "$ENV{'HOME'}/.kiwirc";
 our $ConfigName    = "config.xml";
