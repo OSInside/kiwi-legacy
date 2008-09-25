edd5664ffa8a053932ecc47e771c632c14a5f261
Jan Bornschlegel <jcborn@novell.com>
2008-09-25 11:35:18 +0000

    - fixed empty name bug - added some access methods - fixed access method getPluginList - added enambleAll method
    git-svn-id: https://svn.berlios.de/svnroot/repos/kiwi/kiwi-head@1584 92bc7f66-5d17-0410-ac0d-e081219a1bb3

--- modules/KIWIRepoMetaHandler.pm
+++ modules/KIWIRepoMetaHandler.pm
@@ -35,7 +35,7 @@ sub new
     m_collect	=> undef,     # phone back to KIWICollect (getMediaNumbers and the like)
     m_handlers	=> {},	      # list of specialised handlers
     m_medianame	=> undef,     # name of the media (CD, DVD, ...)
-    m_baseurl	=> undef,     # base url where $MEDIUMNAME$NR reside
+    m_baseurl	=> undef,     # base url where $MEDIUMNAME$NR reside (m_united that is)
   };
   bless ($this, $class);
 
@@ -44,7 +44,7 @@ sub new
   if(not defined($this->{m_collect})) {
     return undef; # rock hard get outta here: caller must check retval anyway
   }
-  $this->{m_unitedir}	= $this->{m_collect}->unitedDir();
+  #$this->{m_unitedir}	= $this->{m_collect}->unitedDir();
   #$this->{m_logger}	= $this->{m_collect}->logger();
 
   $this->gossip("Created $class object successfully.");
@@ -146,10 +146,13 @@ sub loadPlugins
   my $retval = 0;
   my $dir = shift;
   if(not defined($dir)) {
-    return $retval;
+    $dir = $this->collect()->productData()->getOpt("PLUGIN_DIR");
+    if(not defined($dir)) {
+      return $retval;
+    }
   }
-  # remove annoying trailing slashes:
-  #$dir =~ s{(.*)/+$}{$1};
+
+  my $inidir = $this->collect()->productData()->getOpt("INI_DIR");
   unshift @INC, $dir;
   if(not opendir(PLUGINDIR, "$dir")) {
     $this->gossip("loadPlugins: cannot open directory $dir");
@@ -159,18 +162,36 @@ sub loadPlugins
   my @plugins = readdir(PLUGINDIR);
   closedir(PLUGINDIR);
 
+  # fish out plugins:
+  my %plugins;
   foreach my $p(@plugins) {
-    chomp $p;
-    next if(-d "$p");
-    next if($p !~ m{.*Plugin[.]pm});
+    chomp($p);
+    next if( -d "$p");
+    if($p =~m{(.*Plugin)\.pm}) {
+      my $prefix;
+      if(defined($inidir)) {
+	$prefix = "$inidir/";
+      }
+      else {
+	$prefix = "$dir/";
+      }
+      if( -f "$prefix$1.ini") {
+	$plugins{$1} = "$prefix$1.ini";
+      }
+      else {
+	$this->collect()->logger()->warning("[W] loadPlugins: no ini file found for plugin <$1>, skipping\n");
+      }
+    }
+  }
 
-    my $loadsuccess = $this->loadPlugin("$dir/$p");
+  foreach my $p(keys(%plugins)) {
+    my $loadsuccess = $this->loadPlugin("$dir/$p", $plugins{$p});
     if($loadsuccess == 1) {
       $this->gossip("loadPlugins: loaded plugin $p from url $dir successfully.");
       $retval++;
     }
     else {
-      $this->gossip("loadPlugins: failed to load plugin $p from url $dir!");
+      $this->collect()->logger()->error("[E] loadPlugins: failed to load plugin <$p> from url <$dir>!");
     }
   }
 
@@ -180,7 +201,7 @@ sub loadPlugins
 
 
 #...
-# loadPlugin(FILE)
+# loadPlugin(plugin, inifile)
 # - load a specific plugin and do all the checks
 #   (required packages, directories, order number free etc.)
 # return 1 on success, 0 on failure
@@ -191,28 +212,36 @@ sub loadPlugin
   my $retval = 0;
 
   my $file = shift;
+  if($file =~ m{^.*/[a-zA-Z_-]+$}) {
+    $file .= ".pm";
+  }
   if(not(defined($file) and -f $file)) {
-    $this->{m_collect}->logger()->error("loadPlugin: file=$file maybe not readable");
+    $this->{m_collect}->logger()->error("[E] loadPlugin: file=<$file> maybe not readable");
     return $retval;
   }
 
   $file =~ m{(.*)/(.*)([.]pm)$};
   my $plugin = $2;
   if(not defined($plugin)) {
-    $this->{m_collect}->logger()->error("loadPlugin: something in regexp broken: $file =~ m{(.*)/(.*)([.]pm)$}...?");
+    $this->{m_collect}->logger()->error("[E] loadPlugin: something in regexp broken: $file =~ m{(.*)/(.*)([.]pm)$}...?");
     return $retval;
   }
 
   eval "require $plugin";
   if($@) {
-    $this->{m_collect}->logger()->error("loadPlugin: loading $plugin failed");
+    $this->{m_collect}->logger()->error("[E] loadPlugin: loading <$plugin> failed");
   }
   else {
-    my $object = ($plugin)->new($this);
-    my $addsuccess = $this->_addPlugin($object);
-    #my $addsuccess = $this->_addPlugin(new($plugin, $this));
-    if($addsuccess) {
-      $retval = 1;
+    my $inifile = shift;
+    if(!$inifile) {
+      $this->{m_collect}->logger()->error("[E] can't load inifile <$inifile> for plugin <$plugin>");
+    }
+    else {
+      my $object = ($plugin)->new($this, $inifile);
+      my $addsuccess = $this->_addPlugin($object);
+      if($addsuccess) {
+	$retval = 1;
+      }
     }
   }
   return $retval;
@@ -230,18 +259,20 @@ sub _addPlugin
   my $plugin = shift;
 
   my $order = $plugin->order();
-  #my $name  = $plugin->name();
-  #my @dirs  = $plugin->requiredDirs();
-  #my @reqs  = $plugin->requires();
-  #my @desc  = $plugin->description();
 
   if(not defined($order)) {
-    $this->{m_collect}->logger()->info("Undefined order of plugin ".$plugin->name());
+    my $n = $plugin->name();
+    if(not defined($n)) {
+      $n = "Name not set";
+    }
+    $this->{m_collect}->logger()->info("[I] Undefined order of plugin <$n>");
   }
   else {
     if(defined($this->{m_handlers}->{$order})) {
       # we have a problem: (TODO in the future)
-      die "Can't handle mutliple occurences of ordernumbers yet! See next version!";
+      $this->collect()->logger()->error("[E] Can't handle multiple occurance of order!");
+      return $retval;
+      #die "Can't handle mutliple occurences of ordernumbers yet!";
     }
     else {
       $this->{m_handlers}->{$order} = $plugin;
@@ -266,13 +297,25 @@ sub getPlugin
   }
   else {
     if(not defined($this->{m_handlers}->{$index})) {
-      $this->{m_collect}->logger()->warning("no plugin defined with index $index");
+      $this->collect()->logger()->warning("[W] no plugin defined with index <$index>\n");
       return undef;
     }
     else {
       return $this->{m_handlers}->{$index};
     }
   }
+}
+
+
+
+sub getPluginList
+{
+  my $this = shift;
+  if(not ref($this)) {
+    return undef;
+  }
+
+  return %{$this->{m_handlers}};
 }
 
 
@@ -286,7 +329,7 @@ sub createMetadata
   my $retval = 0;
   # execute all registered and activated plugins:
 
-  foreach my $order(sort {$a lt $b } keys(%{$this->{m_handlers}})) {
+  foreach my $order(sort {$a <=> $b } keys(%{$this->{m_handlers}})) {
     if($this->{m_handlers}->{$order}->ready()) {
       $this->{m_handlers}->{$order}->execute();
     }
@@ -294,6 +337,42 @@ sub createMetadata
       $this->gossip("Plugin ".$this->{m_handlers}->{$order}->name()." is not activated yet!");
     }
   }
+}
+
+
+
+sub enableAllPlugins
+{
+  my $this = shift;
+  if(not ref $this) {
+    return undef;
+  }
+
+  foreach(values(%{$this->{m_handlers}})) {
+    $_->ready(1);
+  }
+}
+
+
+
+sub enablePlugins
+{
+  my $this = shift;
+  if(not ref $this) {
+    return undef;
+  }
+
+  my @enable = @_;
+  if(not @enable) {
+    return undef;
+  }
+
+  my $retval = 0; # number of enabled plugins
+  foreach(@enable) {
+    $this->{m_handlers}->{$_}->ready(1);
+    $retval++;
+  }
+  return $retval;
 }
 
 
