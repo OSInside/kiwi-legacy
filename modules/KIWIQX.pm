6711d9cdba67dfe4c7e60c2486a1ebe0977f4fba
Marcus Schaefer <ms@novell.com>
2008-10-20 09:44:34 +0000

    - added file check before calling commands with qxx
    git-svn-id: https://svn.berlios.de/svnroot/repos/kiwi/kiwi-head@1653 92bc7f66-5d17-0410-ac0d-e081219a1bb3

--- modules/KIWIQX.pm
+++ modules/KIWIQX.pm
@@ -49,11 +49,42 @@ sub new {
 #------------------------------------------
 sub qxx ($) {
 	# ...
-	# Activate execution logging
+	# Activate execution logging. The function also checks
+	# if the first name evaluated as program name can be
+	# found in the environment using the which command.
+	# Please note if a command chain is given only the first
+	# item is checked which means subsequent calls in the
+	# chain might fail unnoticed
 	# ---
 	my $cmd = shift;
+	my @prg = "";
 	$cmd =~ s/^ +//g;
 	$cmd =~ s/ +$//g;
+	@prg = split (/\s+/,$cmd);
+	#==========================================
+	# Try to find program name in PATH
+	#------------------------------------------
+	my $prog = qx (/usr/bin/which $prg[0]); chomp ($prog);
+	my $exit = $?;
+	my $code = $? >> 8;
+	if ($exit == -1) {
+		$main::kiwi -> loginfo ("EXEC [Failed to call /usr/bin/which: $!]\n");
+		$main::BT.=cluck ($main::TT.$main::TL++);
+		return $exit;
+	}
+	if ($code != 0) {
+		$main::kiwi -> loginfo ("EXEC [Can't find ".$prg[0]."]\n");
+		$main::BT.=cluck ($main::TT.$main::TL++);
+		return $exit;
+	}
+	if (! -x $prog) {
+		$main::kiwi -> loginfo ("EXEC [Program $prog not an executable]\n");
+		$main::BT.=cluck ($main::TT.$main::TL++);
+		return 0xffff;
+	}
+	#==========================================
+	# Call command line
+	#------------------------------------------
 	if (defined $main::kiwi) {
 		$main::kiwi -> loginfo ("EXEC [$cmd]\n");
 	}
