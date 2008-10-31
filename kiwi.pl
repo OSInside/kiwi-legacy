d27cefca250402e756d97e2daeb324ab95343ff4
Marcus Schaefer <ms@novell.com>
2008-10-31 17:08:38 +0000

    - v2.99 - fixed usbboot to check for LOCAL_BOOT correctly - fixed suse-11.1/suse-live-stick earlyxdm meta data
    git-svn-id: https://svn.berlios.de/svnroot/repos/kiwi/kiwi-head@1711 92bc7f66-5d17-0410-ac0d-e081219a1bb3

--- kiwi.pl
+++ kiwi.pl
@@ -44,7 +44,7 @@ use KIWITest;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "2.98";
+our $Version       = "2.99";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $openSUSE      = "http://download.opensuse.org/repositories/";
