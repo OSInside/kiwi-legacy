c82053fc0829c7071117367a8d8b78052c9d37ab
Marcus Schaefer <ms@novell.com>
2009-02-16 09:34:11 +0000

    - v3.18 - make sure the OEM repartition code leaves an active flaged   boot partition at the end. This might be required for some BIOS   implementation to be able to boot from the device (bnc #475664)
    git-svn-id: https://svn.berlios.de/svnroot/repos/kiwi/kiwi-head@1907 92bc7f66-5d17-0410-ac0d-e081219a1bb3

--- kiwi.pl
+++ kiwi.pl
@@ -44,7 +44,7 @@ use KIWITest;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "3.17";
+our $Version       = "3.18";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $openSUSE      = "http://download.opensuse.org";
