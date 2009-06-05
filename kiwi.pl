da485a7f593959816ae36fefdb37a1a2d27c35b2
Marcus Schäfer <ms@novell.com>
2009-06-05 10:25:35 +0200

    - v3.56 - fixed missing xen bootprofile in vmx and oem (bnc #510276) - added ec2 bootkernel profile for vmx,oem,xen (bnc #510276)

--- kiwi.pl
+++ kiwi.pl
@@ -44,7 +44,7 @@ use KIWITest;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "3.55";
+our $Version       = "3.56";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $openSUSE      = "http://download.opensuse.org";
