b822dbc8f0aa7b7bd5091980ce7a53dd2f8e8a4b
Marcus Schäfer <ms@novell.com>
2010-06-08 17:29:01 +0200

    - v4.40 released

--- kiwi.pl
+++ kiwi.pl
@@ -45,7 +45,7 @@ use KIWITest;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "4.39";
+our $Version       = "4.40";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $openSUSE      = "http://download.opensuse.org";
