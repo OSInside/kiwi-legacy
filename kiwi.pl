44324fe47caada053959b86a4d974f7550c12413
Marcus Schäfer <ms@novell.com>
2010-07-19 10:47:50 +0200

    - v4.49 released

--- kiwi.pl
+++ kiwi.pl
@@ -46,7 +46,7 @@ use KIWIImageFormat;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "4.48";
+our $Version       = "4.49";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $openSUSE      = "http://download.opensuse.org";
