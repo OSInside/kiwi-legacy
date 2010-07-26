e55f81ef3a247f47fff1bf4119e9011ed24ae569
Marcus Schäfer <ms@novell.com>
2010-07-26 16:46:09 +0200

    - v4.51 released

--- kiwi.pl
+++ kiwi.pl
@@ -46,7 +46,7 @@ use KIWIImageFormat;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "4.50";
+our $Version       = "4.51";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $openSUSE      = "http://download.opensuse.org";
