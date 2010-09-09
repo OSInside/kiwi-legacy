e547b8534472794bc2f12a7f9d8ee129d128c205
Marcus Schäfer <ms@novell.com>
2010-09-09 15:33:28 +0200

    - allow the qeth parameters to be set via the kernel   commandline too for the s390/suse-SLE11-JeOS image.   Detailed information about kernel parameters for this   image can be found in the s390/suse-SLE11-JeOS/README

--- template/s390/suse-SLE11-JeOS/root/etc/init.d/kiwi/dasd/01-network.sh
+++ template/s390/suse-SLE11-JeOS/root/etc/init.d/kiwi/dasd/01-network.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 # s390 network hardware setup
 # ----
-qeth_configure -p FE08 -l 0.0.0800 0.0.0801 0.0.0802 1
+#======================================
+# Functions...
+#--------------------------------------
+. /include
+
+qeth_port=VSWL2
+qeth_read=0.0.0800
+qeth_write=0.0.0801
+qeth_ctrl=0.0.0802
+qeth_up=1
+
+#======================================
+# Include kernel parameters
+#--------------------------------------
+includeKernelParameters
+
+#======================================
+# Bring the device online
+#--------------------------------------
+qeth_configure -p $qeth_port -l $qeth_read $qeth_write $qeth_ctrl $qeth_up
 
