b0c6f445643ebb72a524e1a679a2386d9b793956
Marcus Schäfer <ms@novell.com>
2010-09-16 15:44:49 +0200

    - v4.60 released - rebuild documentation

--- kiwi.pl
+++ kiwi.pl
@@ -46,7 +46,7 @@ use KIWIImageFormat;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "4.59";
+our $Version       = "4.60";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $openSUSE      = "http://download.opensuse.org";
