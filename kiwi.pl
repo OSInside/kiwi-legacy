b1ebc331415041c60589dc693f6725f5897f90ba
Marcus Schäfer <ms@novell.com>
2010-12-10 10:38:43 +0100

    - v4.68 released

--- kiwi.pl
+++ kiwi.pl
@@ -47,7 +47,7 @@ use KIWIXMLValidator;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "4.67";
+our $Version       = "4.68";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $openSUSE      = "http://download.opensuse.org";
