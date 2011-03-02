caaa89e8c7eb31705802807864eaaaca56988573
Marcus Schäfer <ms@novell.com>
2011-03-02 11:16:40 +0100

    - v4.80 released

--- kiwi.pl
+++ kiwi.pl
@@ -47,7 +47,7 @@ use KIWIXMLValidator;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "4.79";
+our $Version       = "4.80";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $openSUSE      = "http://download.opensuse.org";
