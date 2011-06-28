94df8551b15eeb72cc36a68a8b19c6d3bb1b44d2
Marcus Schäfer <ms@novell.com>
2011-06-28 17:16:31 +0200

    - allow firstboot and autoyast service to be enabled together (bnc #700105)

--- modules/KIWIRoot.pm
+++ modules/KIWIRoot.pm
@@ -960,6 +960,7 @@ sub setup {
 	my $imageDesc = $this->{imageDesc};
 	my $manager   = $this->{manager};
 	my $data;
+	my $status;
 	#======================================== 
 	# Consistency check
 	#----------------------------------------
@@ -1125,13 +1126,15 @@ sub setup {
 	#========================================
 	# check for yast firstboot setup file
 	#----------------------------------------
-	my $status = $configure -> setupFirstBootYaST();
-	if ($status eq "skipped") {
-		$status = $configure -> setupAutoYaST();
+	$status = $configure -> setupFirstBootYaST();
+	if ($status eq "failed") {
+		return undef;
 	}
-	if ($status eq "skipped") {
-		$status = $configure -> setupFirstBootAnaconda();
+	$status = $configure -> setupAutoYaST();
+	if ($status eq "failed") {
+		return undef;
 	}
+	$status = $configure -> setupFirstBootAnaconda();
 	if ($status eq "failed") {
 		return undef;
 	}
