ebd8893921c64778cfbbfc49f1a949aeeb024ac7
Marcus Schäfer <ms@novell.com>
2011-07-29 11:23:00 +0200

    - v4.94.1 released

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -40,7 +40,7 @@ sub new {
 	# Globals (generic)
 	#------------------------------------------
 	my %data;
-	$data{Version}         = "4.93.3";
+	$data{Version}         = "4.94.1";
 	$data{Publisher}       = "SUSE LINUX Products GmbH";
 	$data{Preparer}        = "KIWI - http://kiwi.berlios.de";
 	$data{ConfigName}      = "config.xml";
