bd39d43cdad33d7ec7bb8b9b1fbe2c248217b4ea
Marcus Schäfer <ms@novell.com>
2011-08-30 16:09:47 +0200

    - make sure $prio variable is initialized empty for   each repo validation (bnc #714927)

--- modules/KIWIManager.pm
+++ modules/KIWIManager.pm
@@ -779,7 +779,6 @@ sub setupInstallationSource {
 	if ($manager eq "zypper") {
 		my @zypper = @{$this->{zypper}};
 		my $stype = "private";
-		my $prio;
 		qxx ("rm -f $dataDir/*.repo");
 		if (! $chroot) {
 			$stype = "public";
@@ -795,6 +794,7 @@ sub setupInstallationSource {
 		foreach my $alias (keys %{$source{$stype}}) {
 			my @sopts = @{$source{$stype}{$alias}};
 			my @zopts = ();
+			my $prio;
 			foreach my $opt (@sopts) {
 				next if ! defined $opt;
 				$opt =~ /(.*?)=(.*)/;
