6c27c04608162a1af896cdc13223943dc969c9ef
Marcus Schäfer <ms@suse.de>
2011-10-17 16:23:17 +0200

    - v4.98.2 released

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -40,7 +40,7 @@ sub new {
 	# Globals (generic)
 	#------------------------------------------
 	my %data;
-	$data{Version}         = "4.98.1";
+	$data{Version}         = "4.98.2";
 	$data{Publisher}       = "SUSE LINUX Products GmbH";
 	$data{Preparer}        = "KIWI - http://kiwi.berlios.de";
 	$data{ConfigName}      = "config.xml";
