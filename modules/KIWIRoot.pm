749c07851877e332d512325219618e4571bfe3e5
Marcus Schäfer <ms@suse.de>
2011-11-09 16:34:36 +0100

    - if an obs:// path can't be resolved, turn it into an opensuse path   and try again. That way you can use one URI to build the image   inside the buildservice and also outside with network access

--- modules/KIWIRoot.pm
+++ modules/KIWIRoot.pm
@@ -115,9 +115,19 @@ sub new {
 		if ($publics_url =~ /^\//) {
 			my ( $publics_url_test ) = bsd_glob ( $publics_url );
 			if (! -d $publics_url_test) {
-				$kiwi ->warning ("local URL path not found: $publics_url_test");
-				$kiwi ->skipped ();
-				next;
+				if ($source =~ /^obs:\//) {
+					$source =~ s/^obs/opensuse/;
+					$publics_url = $urlHandler -> normalizePath ($source);
+					if ($publics_url =~ /^\//) {
+						next;
+					}
+				} else {
+					$kiwi ->warning (
+						"local URL path not found: $publics_url_test"
+					);
+					$kiwi ->skipped ();
+					next;
+				}
 			}
 		}
 		my $private_url = $publics_url;
