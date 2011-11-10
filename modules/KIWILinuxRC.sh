20b09a54a5a2e7194e0bf5e062b7f44819d46e1a
Marcus Schäfer <ms@suse.de>
2011-11-10 11:14:28 +0100

    - fixed parameter quoting in setupBootLoader (bnc #729315)

--- modules/KIWILinuxRC.sh
+++ modules/KIWILinuxRC.sh
@@ -1068,7 +1068,7 @@ function setupBootLoader {
 	local arch=`uname -m`
 	local para=""
 	while [ $# -gt 0 ];do
-		para="$para \"$1\""
+		para="$para '$1'"
 		shift
 	done
 	case $arch-$loader in
@@ -1137,7 +1137,7 @@ function setupBootLoaderRecovery {
 	local arch=`uname -m`
 	local para=""
 	while [ $# -gt 0 ];do
-		para="$para \"$1\""
+		para="$para '$1'"
 		shift
 	done
 	case $arch-$loader in
