4e144f764dd8602c4b8dbb80f3512d6e256ab82f
Robert Schweikert <rjschwei@suse.com>
2011-11-11 16:48:58 -0500

    - add collection of perlcritic exceptions to the statistics collector - change collector such that only the next scan time is written and   not he whole "old" content that does not change. New data is added to the   file at the end - add the doc stats collector provided by Thomas, this is not yet integrated,   but I thought we shouldn't loose the code - register the collector code in the hook

--- tests/qa/stats/collector.pl
+++ tests/qa/stats/collector.pl
@@ -49,25 +49,22 @@ push @modules, glob "$FindBin::Bin/../../unit/*.t";
 push @modules, glob "$FindBin::Bin/../../unit/lib/Test/*.pm";
 push @modules, glob "$FindBin::Bin/../../unit/lib/Common/*.pm";
 
-# Read existing stats since I cannot figure out how to just overwrite the
-# first line :(
-open($STATS, '<', $statsFileName) ||
+open($STATS, '+<', $statsFileName) ||
   die "Could not open '$statsFileName'\n";
-my (@lines) = <$STATS>;
-close $STATS;
-
-open($STATS, '>', $statsFileName) || die "Could not open '$statsFileName'\n";
+# Mark time when the statistics should be collected again
 print $STATS "#NEXT_SCAN:$nextScan\n";
-shift @lines;
-print $STATS @lines;
+# Write new data at the end
+seek $STATS, 0, 2;
 print $STATS "DATE:$curTime\n";
 # Collect statistics
-my $numFiles  = 0;
-my $numTFiles = 0;
-my $totalCCN  = 0.0;
-my $totalTCCN = 0.0;
-my $totalLOC  = 0;
-my $totalTLOC = 0;
+my $critExcept  = 0;
+my $critExceptT = 0;
+my $numFiles    = 0;
+my $numTFiles   = 0;
+my $totalCCN    = 0.0;
+my $totalTCCN   = 0.0;
+my $totalLOC    = 0;
+my $totalTLOC   = 0;
 my $chld_in;
 my $chld_out;
 for my $fl (@modules) {
@@ -80,8 +77,9 @@ for my $fl (@modules) {
     }
     my $fname = basename($fl);
     print $STATS "\tFILE:$fname\n";
-    my $res = open2($chld_out, $chld_in, 'perlcritic', '--statistics-only',
+    my $pid = open2($chld_out, $chld_in, 'perlcritic', '--statistics-only',
                     '-severity', '1', "$fl");
+    waitpid( $pid, 0 );
     while (<$chld_out>) {
         # Extract line of code info an process
         if ($_ =~ /\s*(\d*)\s*lines of Perl code.$/) {
@@ -113,18 +111,45 @@ for my $fl (@modules) {
             print $STATS "\t\t$levelID:$violations\n";
         }
     }
+    # Count the lines of code marked as critic exception
+    my $MOD;
+    open ($MOD, '<', $fl) || die "Could not open $fl for stats collection";
+    my @lines = <$MOD>;
+    close $MOD;
+    my $exceptCnt = 0;
+    my $exceptBlk;
+    for my $ln (@lines) {
+        if ($ln =~ /^\s*## no critic\s*$/) {
+            # Beginning of a critic exception block
+            $exceptBlk = 1;
+            next;
+        }
+        if ($ln =~ /^\s*## use critic\s*$/) {
+            # End of a critic exception block
+            $exceptBlk = undef;
+            next;
+        }
+        if ($exceptBlk or $ln =~ /\s*.*;\s*## no critic\s*$/) {
+            $exceptCnt += 1;
+        }
+    }
+    if ($isTest) {
+        $critExceptT += $exceptCnt;
+    } else {
+        $critExcept += $exceptCnt;
+    }
+    print $STATS "\t\tCEC:$exceptCnt\n";
 }
 # Write out summary data
 print $STATS "TOTAL_LOC:$totalLOC\n";
 print $STATS "NUMBER_FILES:$numFiles\n";
 my $avg = $totalCCN / $numFiles;
 print $STATS "AVG_CCN:$avg\n";
+print $STATS "CRITIC_EXCEPT_LOC:$critExcept\n";
 print $STATS "TOTAL_TEST_LOC:$totalTLOC\n";
 print $STATS "NUMBER_TEST_FILES:$numTFiles\n";
 $avg = $totalTCCN / $numTFiles;
 print $STATS "AVG_TEST_CCN:$avg\n";
-# Update the scan time
-#seek $STATS, 0, 0;
-#print $STATS "#NEXT_SCAN:$nextScan\n";
+print $STATS "CRITIC_EXCEPT_TEST_LOC:$critExceptT\n";
 close $STATS;
 
