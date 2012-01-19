0e459eec064f3b471eaa914d92855baf8774303c
Marcus Schäfer <ms@suse.de>
2012-01-19 11:41:09 +0100

    - v4.85.10 released

--- kiwi.pl
+++ kiwi.pl
@@ -50,7 +50,7 @@ use KIWIXMLValidator;
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "4.85.9";
+our $Version       = "4.85.10";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $ConfigFile    = "$ENV{'HOME'}/.kiwirc";
