0a6f442e4056c1367f857815ecc9ca3f2315d3bd
Robert Schweikert <rjschwei@suse.com>
2012-05-16 10:41:44 -0400

    - update the OpenNebula example   ~ Adjustments for OpenNebula version 3.4.1 release   ~ Minor script fixes

--- doc/examples/extras/suse-11.3/suse-nebula-cloud/cloud_cloud/config.sh
+++ doc/examples/extras/suse-11.3/suse-nebula-cloud/cloud_cloud/config.sh
@@ -3,7 +3,7 @@
 # FILE          : config.sh
 #----------------
 # PROJECT       : OpenSuSE KIWI Image System
-# COPYRIGHT     : (c) 2011 SUSE LINUX Products GmbH. All rights reserved
+# COPYRIGHT     : (c) 2012 SUSE LINUX Products GmbH. All rights reserved
 #               :
 # AUTHOR        : Robert Scwheikert <rjschwei@suse.com>
 #               :
@@ -45,11 +45,14 @@ suseInsertService libvirtd
 baseUpdateSysConfig /etc/sysconfig/network/dhcp DHCLIENT_BIN dhclient
 # Disable IPv6
 baseUpdateSysConfig /etc/sysconfig/network/dhcp DHCLIENT6_BIN /bin/false
-echo “alias net-pf-10 off” >> /etc/modprobe.conf.local
-echo “alias ipv6 off” >> /etc/modprobe.conf.local
+echo "alias net-pf-10 off" >> /etc/modprobe.conf.local
+echo "alias ipv6 off" >> /etc/modprobe.conf.local
 sed -i "s/#net.ipv6.conf.all.disable_ipv6 = 1/net.ipv6.conf.all.disable_ipv6 = 1" /etc/sysctl.conf
 # Configure libvirt settings
 sed -i "s/#listen_tcp = 1/listen_tcp = 1/" /etc/libvirt/libvirtd.conf
+sed -i "s/#dynamic_ownership = 1/dynamic_ownership = 0/" /etc/libvirt/qemu.conf
+sed -i "s/#user = \"root\"/user = \"oneadmin\"/" /etc/libvirt/qemu.conf
+sed -i "s/#group = \"root\"/group = \"cloud\"/" /etc/libvirt/qemu.conf
 
 suseConfig
 
