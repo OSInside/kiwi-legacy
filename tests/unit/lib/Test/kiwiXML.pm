4c424c78d531e836a3518a8cace6eab2272481b6
Robert Schweikert <rjschwei@suse.com>
2012-05-17 11:23:08 -0400

    - unit test for getVMwareConfig method of XML object

--- tests/unit/lib/Test/kiwiXML.pm
+++ tests/unit/lib/Test/kiwiXML.pm
@@ -1266,6 +1266,46 @@ sub test_getSplitTempFiles {
 	$this -> assert_array_equal(\@expectedNames, \@tmpFiles);
 }
 
+#==========================================
+# test_getVMwareConfig
+#------------------------------------------
+sub test_getVMwareConfig {
+	# ...
+	# Verify proper return of VMWare configuration data
+	# ---
+	my $this = shift;
+	my $kiwi = $this -> {kiwi};
+	my $confDir = $this->{dataDir} . 'vmwareConfigSettings';
+	my $xml = new KIWIXML(
+		$this -> {kiwi}, $confDir, undef, undef,$this->{cmdL}
+	);
+	my %vmConfig = $xml -> getVMwareConfig();
+	my $msg = $kiwi -> getMessage();
+	$this -> assert_str_equals('No messages set', $msg);
+	my $msgT = $kiwi -> getMessageType();
+	$this -> assert_str_equals('none', $msgT);
+	my $state = $kiwi -> getState();
+	$this -> assert_str_equals('No state set', $state);
+	# Test these conditions last to get potential error messages
+	$this -> assert_str_equals('x86_64', $vmConfig{vmware_arch});
+	$this -> assert_str_equals('2', $vmConfig{vmware_cdid});
+	$this -> assert_str_equals('ide', $vmConfig{vmware_cdtype});
+	my @expectedOpts = qw / ola pablo /;
+	$this -> assert_array_equal(\@expectedOpts, $vmConfig{vmware_config});
+	$this -> assert_str_equals('1', $vmConfig{vmware_diskid});
+	$this -> assert_str_equals('scsi', $vmConfig{vmware_disktype});
+	$this -> assert_str_equals('sles-64', $vmConfig{vmware_guest});
+	$this -> assert_str_equals('7', $vmConfig{vmware_hwver});
+	$this -> assert_str_equals('1024', $vmConfig{vmware_memory});
+	$this -> assert_str_equals('2', $vmConfig{vmware_ncpus});
+	my $nicConfig = $vmConfig{vmware_nic};
+	my %nicSetup = %$nicConfig;
+	my $nicInfo = $nicSetup{eth0};
+	$this -> assert_not_null($nicInfo);
+	my %nicDetails = %$nicInfo;
+	$this -> assert_str_equals('e1000', $nicDetails{drv});
+	$this -> assert_str_equals('dhcp', $nicDetails{mode});
+}
 
 #==========================================
 # test_packageManagerInfoHasConfigValue
