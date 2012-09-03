945f4ad5fd77d2af0c0c734f44b9f0e305274fc4
Marcus Schäfer <ms@suse.de>
2012-09-03 15:44:00 +0200

    - v5.03.46 released

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -50,7 +50,7 @@ sub new {
 	# Globals (generic)
 	#------------------------------------------
 	my %data;
-	$data{Version}         = "5.03.45";
+	$data{Version}         = "5.03.46";
 	$data{Publisher}       = "SUSE LINUX Products GmbH";
 	$data{Preparer}        = "KIWI - http://kiwi.berlios.de";
 	$data{ConfigName}      = "config.xml";
