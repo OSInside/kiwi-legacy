a082edff2315207b816fc69c53db0a481cbba140
Marcus Schäfer <ms@suse.de>
2012-10-19 16:06:49 +0200

    - v5.04.19 released

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -50,7 +50,7 @@ sub new {
 	# Globals (generic)
 	#------------------------------------------
 	my %data;
-	$data{Version}         = "5.04.18";
+	$data{Version}         = "5.04.19";
 	$data{Publisher}       = "SUSE LINUX Products GmbH";
 	$data{Preparer}        = "KIWI - http://opensuse.github.com/kiwi";
 	$data{ConfigName}      = "config.xml";
