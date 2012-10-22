28b88d0436c0bd98b6ebb7b0c0ace193bd9d24a3
madanyang <toganm@opensuse.org>
2012-10-22 12:20:28 +0200

    Emacs mode fix
    Fix typos in the suse-start-kiwi-xmllocator
    Rather than blindly adding suffixes to nxml-mode just add kiwi suffix
    to include to the auto-mode-list

--- editing/suse-start-kiwi-mode.el
+++ editing/suse-start-kiwi-mode.el
@@ -1,6 +1,7 @@
-(setq auto-mode-alist
-(cons '("\\.\\(xml\\|kiwi\\|xsl\\|rng\\|xhtml\\)\\'" . nxml-mode)
-auto-mode-alist))
+;; For file names ending with kiwi use the nxml-mode
+;; and locate the schemas in kiwi editing directory
 
-(eval-after-load 'rng-loc
-'(add-to-list 'rnc-schema-locating-files "/usr/share/kiwi/editing/suse-start-kiwi-xmllocator.xml"))
+(progn
+  (add-to-list 'auto-mode-alist '("\\.kiwi" . nxml-mode))
+  (eval-after-load 'rng-loc
+    '(add-to-list 'rng-schema-locating-files "/usr/share/kiwi/editing/suse-start-kiwi-xmllocator.xml")))
