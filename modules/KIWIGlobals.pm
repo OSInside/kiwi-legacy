4bfe8aeb9b628bcca9399aee377d49754ca0c8f3
Marcus Schäfer <ms@suse.de>
2012-11-30 09:43:02 +0100

    - v5.03.59 released

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -50,7 +50,7 @@ sub new {
 	# Globals (generic)
 	#------------------------------------------
 	my %data;
-	$data{Version}         = "5.03.58";
+	$data{Version}         = "5.03.59";
 	$data{Publisher}       = "SUSE LINUX Products GmbH";
 	$data{Preparer}        = "KIWI - http://kiwi.berlios.de";
 	$data{ConfigName}      = "config.xml";
--- rpm/kiwi.spec
+++ rpm/kiwi.spec
@@ -21,7 +21,7 @@ Name:           kiwi
 Summary:        openSUSE - KIWI Image System
 License:        GPL-2.0
 Group:          System/Management
-Version:        5.03.58
+Version:        5.03.59
 Release:        0
 # requirements to build packages
 BuildRequires:  gcc-c++
