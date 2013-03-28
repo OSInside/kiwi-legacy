365a126d1676b37e9c0dff85f936a02208cc2237
Marcus Schäfer <ms@suse.de>
2013-03-28 11:04:08 +0100

    - v5.04.69 released

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -936,7 +936,7 @@ sub _new_instance {
 	# Globals (generic)
 	#------------------------------------------
 	my %data;
-	$data{Version}         = "5.04.68";
+	$data{Version}         = "5.04.69";
 	$data{Publisher}       = "SUSE LINUX GmbH";
 	$data{Preparer}        = "KIWI - http://opensuse.github.com/kiwi";
 	$data{ConfigName}      = "config.xml";
--- rpm/kiwi.spec
+++ rpm/kiwi.spec
@@ -26,7 +26,7 @@ Name:           kiwi
 Summary:        openSUSE - KIWI Image System
 License:        GPL-2.0
 Group:          System/Management
-Version:        5.04.68
+Version:        5.04.69
 Release:        0
 # requirements to build packages
 BuildRequires:  e2fsprogs
