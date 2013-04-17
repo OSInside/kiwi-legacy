77de6bea8ba3b56cda5feb8ad33aa116f56560a8
Marcus Schäfer <ms@suse.de>
2013-04-17 12:09:27 +0200

    - v5.04.80 released

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -936,7 +936,7 @@ sub _new_instance {
 	# Globals (generic)
 	#------------------------------------------
 	my %data;
-	$data{Version}         = "5.04.79";
+	$data{Version}         = "5.04.80";
 	$data{Publisher}       = "SUSE LINUX GmbH";
 	$data{Preparer}        = "KIWI - http://opensuse.github.com/kiwi";
 	$data{ConfigName}      = "config.xml";
