e61e4f5032760b8addb2c2cfac07236bfd761ef6
Marcus Schäfer <ms@suse.de>
2013-05-02 11:45:04 +0200

    - v4.85.104 released

--- kiwi.pl
+++ kiwi.pl
@@ -57,7 +57,7 @@ binmode(STDOUT, ":utf8");
 #============================================
 # Globals (Version)
 #--------------------------------------------
-our $Version       = "4.85.103";
+our $Version       = "4.85.104";
 our $Publisher     = "SUSE LINUX Products GmbH";
 our $Preparer      = "KIWI - http://kiwi.berlios.de";
 our $ConfigFile    = "$ENV{'HOME'}/.kiwirc";
