97a1731ff0272096242b9438bfcff968ea0d849b
Robert Schweikert <rjschwei@suse.com>
2013-06-04 10:13:53 -0400

    - write dvd/cd device entry to vmx file if the device ide is 0 (bnc 822185)

--- modules/KIWIImageFormat.pm
+++ modules/KIWIImageFormat.pm
@@ -1018,7 +1018,7 @@ sub createVMwareConfiguration {
 	#------------------------------------------
 	my $cdtype = $vmdata -> getDVDController();
 	my $cdid = $vmdata -> getDVDID();
-	if ($cdtype && $cdid) {
+	if ($cdtype && defined $cdid) {
 		my $device = $cdtype . $cdid;
 		print $VMWFD $device.':0.present = "true"'."\n";
 		print $VMWFD $device.':0.deviceType = "cdrom-raw"'."\n";
