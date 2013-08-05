a9a23c0fec842e323029707633f84500ee78ec2f
Marcus Schäfer <ms@suse.de>
2013-08-05 12:40:28 +0200

    - fixed getSingleInstSourceSatSolvable() in terms of pattern   handling. the function stores projectxml always with the   the suffix .gz which indicates a compressed version of the   file. But the patterns information file could also contain   a non compressed version of patterns.xml which is then   wrongly used as compressed file (bnc #822247)

--- modules/KIWIXML.pm
+++ modules/KIWIXML.pm
@@ -3630,6 +3630,7 @@ sub getSingleInstSourceSatSolvable {
 	my %patterns;
 	$patterns{"/suse/setup/descr/patterns"} = "patterns";
 	$patterns{"/repodata/patterns.xml.gz"}  = "projectxml";
+	$patterns{"/repodata/patterns.xml"}     = "projectxml";
 	#==========================================
 	# common data variables
 	#------------------------------------------
@@ -3828,7 +3829,11 @@ sub getSingleInstSourceSatSolvable {
 	$count++;
 	foreach my $patt (keys %patterns) {
 		my $name = $patterns{$patt};
-		$destfile = $sdir."/$name-".$count.".gz";
+		if ($patt =~ /\.gz$/) {
+			$destfile = $sdir."/$name-".$count.".gz";
+		} else {
+			$destfile = $sdir."/$name-".$count;
+		}
 		my $ok = KIWIXML::getInstSourceFile ($kiwi,$repo.$patt,$destfile);
 		if (($ok) && ($name eq "patterns")) {
 			#==========================================
@@ -3927,11 +3932,21 @@ sub getSingleInstSourceSatSolvable {
 	#==========================================
 	# create solvable from opensuse xml pattern
 	#------------------------------------------
-	if (glob ("$sdir/projectxml-*.gz")) {
-		foreach my $file (glob ("$sdir/projectxml-*.gz")) {
+	if (glob ("$sdir/projectxml-*")) {
+		my $gzicmd = "gzip -cd ";
+		my $stdcmd = "cat ";
+		my $data;
+		my $code;
+		foreach my $file (glob ("$sdir/projectxml-*")) {
 			$destfile = $sdir."/primary-".$count;
-			my $data = qxx ("gzip -cd $file | rpmmd2solv > $destfile 2>&1");
-			my $code = $? >> 8;
+			if ($file =~ /\.gz$/) {
+				$gzicmd .= $file." ";
+				$data = qxx ("$gzicmd | rpmmd2solv > $destfile 2>&1");
+			} else {
+				$stdcmd .= $file." ";
+				$data = qxx ("$stdcmd | rpmmd2solv > $destfile 2>&1");
+			}
+			$code = $? >> 8;
 			if ($code != 0) {
 				$kiwi -> failed ();
 				$kiwi -> error  ("--> Can't create SaT solvable file");
