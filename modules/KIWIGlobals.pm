b8c78fd3037ea110e54d08d0c9aa897f8492601b
Marcus Schäfer <ms@suse.de>
2013-12-10 20:58:31 +0100

    - v5.06.13 released

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -1471,7 +1471,7 @@ sub _new_instance {
 	# Globals (generic)
 	#------------------------------------------
 	my %data;
-	$data{Version}         = "5.06.12";
+	$data{Version}         = "5.06.13";
 	$data{Publisher}       = "SUSE LINUX GmbH";
 	$data{Preparer}        = "KIWI - http://opensuse.github.com/kiwi";
 	$data{ConfigName}      = "config.xml";
