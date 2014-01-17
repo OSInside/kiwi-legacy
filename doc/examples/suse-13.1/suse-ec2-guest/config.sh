3c62482468a96d55fd8e158d1f6529d1c28b9052
Robert Schweikert <rjschwei@suse.com>
2014-01-17 11:59:30 -0500

    - modify the way we create an EC2 image    + prior to this change the initrd for an EC2 image was created with      mkinitrd as mkinitrd changes got introduced that require a live root      filesystem using mkinitrd is no longer feasible. The same situation      applies as things move to dracut    + we now use a kiwi generated initrd for a vmx image type, and modify      specific files (menu.lst, bootloader setup, inittab) in the image      to make the image ec2 specific    + this change requires the use of different EC2 akis, we need to use      an aki designated for a partitioned image hd00 designation    + using the VMX build type creates a partitioned image, thus we need to      use a different device mapping when creating the ec2-bundle, use      ami=sda instead of ami=sda1 - update the unit tests to reflect      the changes - EC2 XML validation now checks that the image type is VMX instead of a   filesystem    + this is somewhat superfluous as it is more intuitive to build an EC2      image as VMX. However as the previous type requirement was a file system      we want to catch this and let people know if the config is incorrect. - Use GPT partition for EC2    + Amazon is transitioning to expecting GPT, thus we need to follow suit - update the 12.2 and 12.3 examples to reflect the new build type settings   for ec2 images - add an ec2 example for 13.1 - include commit aba2000d946e1cf5d577a625c1cbfcab218fad7d from master   chery-pick failed, thus no separate commit

---
+++ doc/examples/suse-13.1/suse-ec2-guest/config.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#================
+# FILE          : config.sh
+#----------------
+# PROJECT       : OpenSuSE KIWI Image System
+# COPYRIGHT     : (c) 2006 SUSE LINUX Products GmbH. All rights reserved
+#               :
+# AUTHOR        : Marcus Schaefer <ms@suse.de>
+#               :
+# BELONGS TO    : Operating System images
+#               :
+# DESCRIPTION   : configuration script for SUSE based
+#               : operating systems
+#               :
+#               :
+# STATUS        : BETA
+#----------------
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+#======================================
+# Setup baseproduct link
+#--------------------------------------
+suseSetupProduct
+
+#======================================
+# Activate services
+#--------------------------------------
+suseActivateDefaultServices
+suseInsertService amazon
+suseInsertService amazon-late
+suseInsertService boot.device-mapper
+suseInsertService sshd
+suseInsertService sces-client
+
+#======================================
+# Let the DHCP server set the hostname
+#--------------------------------------
+suseConfig
+baseUpdateSysConfig /etc/sysconfig/network/dhcp DHCLIENT_SET_HOSTNAME yes
+
+#======================================
+# From suse-ami-tools
+#--------------------------------------
+suse-ec2-configure --norefresh
+
+#======================================
+# clone runlevel 3 to 4
+#--------------------------------------
+suseCloneRunlevel 4
+
+#======================================
+# Umount kernel filesystems
+#--------------------------------------
+baseCleanMount
+
+exit 0
