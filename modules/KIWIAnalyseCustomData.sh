f9058701f50d13ff67ffcd15be54c30ea55d0176
Marcus Schäfer <ms@suse.de>
2014-02-13 10:45:18 +0100

    - Improve searching for custom files   Make the code more readable, also skip proc/devpts/sysfs filesystems   Along with that search in junks and make sure the xargs limit is not   exceeded. Also reduce the list of searched directories with regards   to the recursive search done by find

--- modules/KIWIAnalyseCustomData.sh
+++ modules/KIWIAnalyseCustomData.sh
@@ -24,70 +24,121 @@ export LC_ALL=C
 #======================================
 # Functions
 #--------------------------------------
-function fileName {
-	echo -n /var/run/$1
-}
-
-function sortUniq {
-	sort -u $1 -o $1
-}
-
-function inputFromFile {
-	local data=$(cat $1 | tr '\n' ' ')
-	echo $data
-}
-
-function prune_rpm {
-	# when searching for names in the rpm database always
-	# ignore special filesystems tmp and root(/)
-	echo -n "^/(\$|tmp|dev|proc|sys|run|lost+found)"
-}
-
-function prune_find {
-	# when searching for files via the find command always
-	# ignore special filesystems and tmp
-	local ignore="/tmp /dev /proc /sys /run lost+found"
-	for i in $ignore;do
-		if [[ $i =~ ^/ ]];then
-			echo -n "-not -path $i "
-		else
-			echo -n "-not -name $i "
+function checkEnv {
+	# check if all tools used in this code exists
+	for tool in find tr sort split cut cat grep rpm diff;do
+		if ! type -p $tool &>/dev/null;then
+			echo "Required tool $tool not found"
+			exit 1
 		fi
 	done
 }
 
+function fileName {
+	# global location for runtime and result files
+	echo -n /tmp/$1
+}
+
+function sortUniq {
+	# sort and uniq the given stream
+	sort -u $1 -o $1
+}
+
+function filter_items {
+	# files and directories matching here are filtered out
+	echo -n "^/(\$|tmp|dev|proc|sys|run|lost\+found|var\/run)"
+}
+
+function filter_filesystem {
+	# filesystems with the given blockid are filtered out
+	for fs in nfs tmpfs proc sysfs devtmpfs devpts;do
+		echo -n "-not -fstype $fs "
+	done
+}
+
+function inputFromFile {
+	# turn lines from file into space separated argument list
+	local data=$(cat $1 | grep -v -E "$(filter_items)" | tr '\n' ' ')
+	echo $data
+}
+
+function quote {
+	# translate '\n' with '\a' leave all other characters untouched
+	# translate '\0' to '\n' to get our lines back
+	tr "\n" "\a" | tr "\0" "\n"
+}
+
 function getRPMManagedData {
-	local dirs_count=0
+	# lookup rpm database for all files and directories
+	local cur_dir
 	local rpm_all_names=$(fileName rpm_all_names)
 	local rpm_dir_names=$(fileName rpm_dir_names)
 	local rpm_dump=$(fileName rpm_dump)
 	if [ ! -e $rpm_dump ];then
-		return 1
+		echo "Failed to dump data from RPM database"
+		exit 1
 	fi
 	# directories start with mode 040...
 	grep ' 040' $rpm_dump | sed -e "s@ [0-9].*@@" |\
-		grep -v -E "$(prune_rpm)" > $rpm_all_names
+		grep -v -E "$(filter_items)" > $rpm_all_names
 	cp $rpm_all_names $rpm_dir_names
 	# all other items different from dir mode
 	grep -v ' 040' $rpm_dump | sed -e "s@ [0-9].*@@" >> $rpm_all_names
+	# remove rpm dump file
 	rm -f $rpm_dump
+	# sort and uniq result files
 	sortUniq $rpm_all_names
+	sortUniq $rpm_dir_names
+	# strip rpm directory list to minimal list for later
+	# recursive search in a find call
+	rm -f ${rpm_dir_names}.stripped
+	while read dir; do
+		if [ ! -z "$cur_dir" ] && [[ $dir =~ ^$cur_dir ]];then
+			continue
+		fi
+		cur_dir=$dir
+		echo $dir >> ${rpm_dir_names}.stripped
+	done < $rpm_dir_names
+	mv ${rpm_dir_names}.stripped $rpm_dir_names
 	return 0
 }
 
 function searchRootFSData {
+	# search files and directories of this system. The search
+	# method is based on the managed RPM directories. This is
+	# faster than recursively searching through the entire '/'
+	# filesystem. This method could skip some of the files
+	# in a way that e.g only /music is detected but not all
+	# files stored inside /music. If something like that happens
+	# depends on the structure of the RPM managed items as well
+	# as on the location people put custom files in. The goal
+	# is not to find each and every custom file but at least
+	# one target to cover all areas where custom files are
+	# stored
 	local rpm_find_names=$(fileName rpm_find_names)
 	local rpm_dir_names=$(fileName rpm_dir_names)
-	# perform search, but don't leave this machine
-	echo $(inputFromFile $rpm_dir_names) -not -fstype nfs $(prune_find) |\
-		xargs find 2>/dev/null > $rpm_find_names
 	# perform the search in root(/) with no recursion (maxdepth 0)
-	find /* -maxdepth 0 -type d $(prune_find) >> $rpm_find_names
+	find /* -maxdepth 0 -type d -print0 | quote |\
+		grep -v -E "$(filter_items)" > $rpm_find_names
+	# perform search in junks, but don't leave this machine
+	split -l 1000 $rpm_dir_names ${rpm_dir_names}_part
+	for part in ${rpm_dir_names}_part*;do
+		find_options="$(inputFromFile $part)"
+		find_options="$find_options $(filter_filesystem)"
+		find_options="$find_options -print0"
+		echo $find_options | xargs -x find 2>/dev/null | quote |\
+			grep -v -E "$(filter_items)" >> $rpm_find_names
+	done
+	# sort and uniq result files
 	sortUniq $rpm_find_names
+	# remove rpm directory and split part files
 	rm -f $rpm_dir_names
+	rm -f ${rpm_dir_names}_part*
 }
 
 function getUnmanagedData {
+	# unmanaged files are the diff between the items in the
+	# RPM database compared to the result of the find call
 	local rpm_unmanaged=$(fileName rpm_unmanaged)
 	local rpm_find_names=$(fileName rpm_find_names)
 	local rpm_all_names=$(fileName rpm_all_names)
@@ -101,6 +152,7 @@ function getUnmanagedData {
 #======================================
 # main
 #--------------------------------------
+checkEnv
 if getRPMManagedData;then
 	searchRootFSData
 	getUnmanagedData
