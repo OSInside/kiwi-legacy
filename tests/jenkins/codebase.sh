932b8aa072e58cec47ac02ab71b501fe203c3a8a
Marcus Schäfer <ms@suse.de>
2014-03-18 09:27:00 +0100

    - Simplify jenkins codebase job script

--- tests/jenkins/codebase.sh
+++ tests/jenkins/codebase.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # runner for unit / validation and critic tests
 #
-# Required: prepare.sh
+# Required:
+# --> prepare.sh
+#
+# Put the following into the shell execution layer at jenkins
+#
+# /home/jenkins/kiwi/kiwi/tests/jenkins/codebase.sh <topic-name>
 #
 topic=$1
-su - jenkins -c "cd /home/jenkins/kiwi/kiwi && git pull"
 su - jenkins -c "cd /home/jenkins/kiwi/kiwi && make $topic"
