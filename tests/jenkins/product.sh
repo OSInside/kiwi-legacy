e648d952e863a5fa8f8ff2be8f63b93b83ac99ba
Marcus Schäfer <ms@suse.de>
2014-04-10 12:22:17 +0200

    - Update jenkins product.sh script   Support a hop host to allow searching for build results

--- tests/jenkins/product.sh
+++ tests/jenkins/product.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
-# runner for testing results of buildservice product builds
+# runner for testing results of buildservice product and image builds
+#
+# the kiwi XML description can be either a product or image definition
+# which results in either a product ISO media containing the RPMs of
+# the distribution or a read-to-use appliance according to the
+# capabilities of the kiwi appliance builder. To simplify the use
+# of this script in jenkins we call all results 'products'
 #
 # Required:
 # --> prepare.sh
-# --> product build setup in OBS:Virtualization:Appliances
+# --> product build setup in some buildservice project
 #
 # Put the following into the shell execution layer at jenkins
 #
-# /home/jenkins/kiwi/kiwi/tests/jenkins/product.sh <product> <repo> <arch>
+# /home/jenkins/kiwi/kiwi/tests/jenkins/product.sh \
+#     <project> <product> <repo> <arch> [ hop-host ]
 #
-# possible product: product-netboot | product-addon
-# possible repo: images_Factory
-# possible arch: i586
+# project e.g: Virtualization:Appliances
+# product e.g: product-netboot
+# repo e.g:    images_Factory
+# arch e.g:    i586
+# hop e.g:     ms@isny.sytes.net
 #
-product=$1
-repo=$2
-arch=$3
-osc rbl Virtualization:Appliances \
-	$product $repo $arch > /tmp/${product}.result
+project=$1
+product=$2
+repo=$3
+arch=$4
+hop=$5
+if [ -z "$hop" ];then
+	osc rbl $project \
+		$product $repo $arch > /tmp/${product}.result
+else
+	su - jenkins -c \
+		"ssh $hop rbl $project $product $repo $arch > /tmp/${product}.result"
+fi
 if [ ! $? = 0 ];then
 	# osc call failed for some reason
 	exit 1
