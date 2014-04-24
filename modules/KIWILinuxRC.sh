3190a2d21ca4311a1e06320eec1d490f433183ac
Marcus Schäfer <ms@suse.de>
2014-04-24 09:04:55 +0200

    - Fixed pxePartitionInputGeneric   In case of a raid setup via pxeboot the required partition type 'fd'   is not explicitly provided in the PART line of the client config.   Instead the raid information is given in an additional RAID variable.   Thus the pxePartitionInputGeneric function has to set the partition   type 'fd' if the RAID variable is set too.

--- modules/KIWILinuxRC.sh
+++ modules/KIWILinuxRC.sh
@@ -9190,7 +9190,7 @@ function pxePartitionInputGeneric {
 			partID=8e
 			pname=lxlvm
 		fi
-		if [ $partID = "fd" ];then
+		if [ $partID = "fd" ] || [ ! -z "$RAID" ];then
 			partID=fd
 			pname=lxroot
 		fi
