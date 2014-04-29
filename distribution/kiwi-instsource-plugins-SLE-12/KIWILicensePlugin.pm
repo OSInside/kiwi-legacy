b64520331728cf9cf5285d79548be49701416df8
Marcus Schäfer <ms@suse.de>
2014-04-29 14:32:18 +0200

    - Update SLE12 product plugin for style/convention   * also fixed code to be perl critic level 3 clean

--- distribution/kiwi-instsource-plugins-SLE-12/KIWILicensePlugin.pm
+++ distribution/kiwi-instsource-plugins-SLE-12/KIWILicensePlugin.pm
@@ -16,75 +16,80 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 #
 ################################################################
-
-
 package KIWILicensePlugin;
 
 use strict;
+use warnings;
 
 use base "KIWIBasePlugin";
-use Data::Dumper;
 
-
-sub new
-{
-  my $class   = shift;
-  my $handler = shift;
-  my $config  = shift;
-
-  my $this = new KIWIBasePlugin($handler);
-  bless ($this, $class);
-
-  my $ini = new Config::IniFiles( -file => "$config" );
-  my $name	= $ini->val('base', 'name'); # scalar value
-  my $order	= $ini->val('base', 'order'); # scalar value
-  my $enable	= $ini->val('base', 'defaultenable'); # scalar value
-
-  # if any of those isn't set, complain!
-  if(not defined($name)
-     or not defined($order)
-     or not defined($enable)
-    ) {
-    $this->logMsg("E", "Plugin ini file <$config> seems broken!");
-    return undef;
-  }
-
-  $this->name($name);
-  $this->order($order);
-  if($enable != 0) {
-    $this->ready(1);
-  }
-
-  return $this;
+sub new {
+	my $class   = shift;
+	my $handler = shift;
+	my $config  = shift;
+	my $this = KIWIBasePlugin -> new ($handler);
+	bless ($this, $class);
+	my $ini = Config::IniFiles -> new (
+		-file => "$config"
+	);
+	my $name = $ini->val('base', 'name');
+	my $order = $ini->val('base', 'order');
+	my $enable = $ini->val('base', 'defaultenable');
+	if (not defined($name)
+		or not defined($order)
+		or not defined($enable)
+	) {
+		$this->logMsg(
+			"E", "Plugin ini file <$config> seems broken!"
+		);
+		return;
+	}
+	$this->name($name);
+	$this->order($order);
+	if ($enable != 0) {
+		$this->ready(1);
+	}
+	return $this;
 }
 
-sub execute
-{
-  my $this = shift;
-
-  for my $cd ($this->collect()->getMediaNumbers()) {
-    next if $cd == 0;
-    ( my $name = $this->collect()->{m_basesubdir}->{$cd} ) =~ s{.*/(.*)/*$}{$1};
-    my $media_base = $this->collect()->{m_united}."/$name";
-    my $media_license_dir = $this->collect()->{m_united}."/".$name.".license";
-    $this->logMsg("I", "Check for licenses on media $media_base");
-    my $licenseArchive = "$media_base/license.tar.gz";
-    if ( -e $licenseArchive ) {
-       $this->logMsg("I", "Extracting license.tar.gz");
-       system("mkdir $media_license_dir && tar xf $licenseArchive -C $media_license_dir") || return 1;
-
-       # add it to repomd metadata
-       if ( -e "$media_base/repodata" ) {
-         $this->logMsg("I", "Adding license.tar.gz to rpm-md data");
-         system("cd $media_base && modifyrepo $licenseArchive repodata") || return 1;
-       }
-    }
-  }
-
-  return 0;
+sub execute {
+	my $this = shift;
+	my $result;
+	for my $cd ($this->collect()->getMediaNumbers()) {
+		next if $cd == 0;
+		my $name = $this->collect()->{m_basesubdir}->{$cd};
+		$name =~ s{.*/(.*)/*$}{$1}x;
+		my $media_base = $this->collect()->{m_united}."/$name";
+		my $media_license_dir = $this->collect()->{m_united}
+			."/".$name.".license";
+		$this->logMsg(
+			"I", "Check for licenses on media $media_base"
+		);
+		my $licenseArchive = "$media_base/license.tar.gz";
+		if ( -e $licenseArchive ) {
+			$this->logMsg("I", "Extracting license.tar.gz");
+			system("mkdir $media_license_dir");
+			$result = $? >> 8;
+			if ($result != 0) {
+				return 1;
+			}
+			system("tar xf $licenseArchive -C $media_license_dir");
+			$result = $? >> 8;
+			if ($result != 0) {
+				return 1;
+			}
+			# add license information to repomd metadata
+			if ( -e "$media_base/repodata" ) {
+				$this->logMsg("I", "Adding license.tar.gz to rpm-md data");
+				system("cd $media_base && modifyrepo $licenseArchive repodata");
+				$result = $? >> 8;
+				if ($result != 0) {
+					return 1;
+				}
+			}
+		}
+	}
+	return 0;
 }
-
-
 
 1;
-
