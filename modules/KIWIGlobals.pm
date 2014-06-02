705c510a0cecc01f38556211847c71e466177687
Marcus Schäfer <ms@suse.de>
2014-06-02 10:40:31 +0200

    - v5.06.105 released

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -1398,7 +1398,7 @@ sub _new_instance {
 	# Globals (generic)
 	#------------------------------------------
 	my %data;
-	$data{Version}         = "5.06.104";
+	$data{Version}         = "5.06.105";
 	$data{Publisher}       = "SUSE LINUX GmbH";
 	$data{Preparer}        = "KIWI - http://opensuse.github.com/kiwi";
 	$data{ConfigName}      = "config.xml";
--- rpm/kiwi.spec
+++ rpm/kiwi.spec
@@ -27,7 +27,7 @@ Url:            http://github.com/openSUSE/kiwi
 Name:           kiwi
 License:        GPL-2.0
 Group:          System/Management
-Version:        5.06.104
+Version:        5.06.105
 Provides:       kiwi-schema = 6.1
 Release:        0
 # requirements to build packages
