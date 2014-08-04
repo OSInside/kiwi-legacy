028c53172c17fb529caa04f5614c72b4f58b6237
Marcus Schäfer <ms@suse.de>
2014-08-04 12:23:14 +0200

    - Fixed build.sh jenkins test   * requires a mandatory arch parameter now

--- tests/jenkins/build.sh
+++ tests/jenkins/build.sh
@@ -8,15 +8,14 @@
 #
 # Put the following into the shell execution layer at jenkins
 #
-# /home/jenkins/kiwi/kiwi/tests/jenkins/build.sh <jeos-name>
+# /home/jenkins/kiwi/kiwi/tests/jenkins/build.sh <jeos-name> <arch>
 #
 # possible jeos-name: see the result of 'kiwi -l'
 #
 jeos=$1
+shift
 arch=$2
-if [ -z "$arch" ];then
-    arch=x86_64
-fi
+shift
 
 /home/jenkins/kiwi/kiwi/kiwi \
     --build /home/jenkins/kiwi/kiwi/template/$arch/$jeos \
