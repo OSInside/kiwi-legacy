b5c6045602ef9000b1a6f054e1a3ad110f02cdef
Marcus Schäfer <ms@suse.de>
2014-09-30 09:59:59 +0200

    Branch created for SLE12, master open for development at version 7

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -1831,7 +1831,7 @@ sub _new_instance {
     # Globals (generic)
     #------------------------------------------
     my %data;
-    $data{Version}         = "5.06.165";
+    $data{Version}         = "7.01.1";
     $data{Publisher}       = "SUSE LINUX GmbH";
     $data{Preparer}        = "KIWI - http://opensuse.github.com/kiwi";
     $data{ConfigName}      = "config.xml";
