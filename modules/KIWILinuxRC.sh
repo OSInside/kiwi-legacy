705ca55178d25e3b20bca61a21fafc8a5085be0b
Marcus Schäfer <ms@suse.de>
2015-03-24 15:30:30 +0100

    Setup kernel symlinks in sle11 elilo compat mode

--- modules/KIWILinuxRC.sh
+++ modules/KIWILinuxRC.sh
@@ -1139,6 +1139,12 @@ description = "$kiwi_oemtitle"
 root = $(getDiskID $imageRootDevice)
 EOF
             #====================================================
+            # setup kernel symlinks
+            #----------------------------------------------------
+            local kversion=$(uname -r)
+            ln -sf /boot/vmlinuz-$kversion /boot/vmlinuz
+            ln -sf /boot/initrd-$kversion /boot/initrd
+            #====================================================
             # copy vendor directory
             #----------------------------------------------------
             cp -a $efipath/BOOT $efipath/$vendor
@@ -1164,7 +1170,6 @@ EOF
             #====================================================
             # create versionized kernel/initrd
             #----------------------------------------------------
-            local kversion=$(uname -r)
             cp -a $efipath/$vendor/vmlinuz $efipath/$vendor/vmlinuz-$kversion
             cp -a $efipath/$vendor/initrd $efipath/$vendor/initrd-$kversion
             #======================================
