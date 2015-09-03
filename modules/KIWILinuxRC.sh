827e290d648472f29b80274b128c350c95cd699e
Jochen Keil <jochen.keil@gmail.com>
2015-09-03 12:37:10 +0200

    Test whether file is executable instead mere existence

--- modules/KIWILinuxRC.sh
+++ modules/KIWILinuxRC.sh
@@ -4267,7 +4267,7 @@ function searchImageISODevice {
     if [ ! -x $isoinfo ]; then
        isoinfo=/usr/lib/genisoimage/isoinfo
     fi
-    if [ ! -e $isoinfo ];then
+    if [ ! -x $isoinfo ];then
         systemException \
             "Can't find isoinfo tool in initrd" \
         "reboot"
