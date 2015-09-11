920092989e8c24b528fc7067862950ae4d0d6ab1
Marcus Schäfer <ms@suse.com>
2015-09-11 09:55:44 +0200

    - v7.03.18 released

--- modules/KIWIGlobals.pm
+++ modules/KIWIGlobals.pm
@@ -1933,7 +1933,7 @@ sub _new_instance {
     # Globals (generic)
     #------------------------------------------
     my %data;
-    $data{Version}         = "7.03.17";
+    $data{Version}         = "7.03.18";
     $data{Publisher}       = "SUSE LINUX GmbH";
     $data{Preparer}        = "KIWI - http://opensuse.github.com/kiwi";
     $data{ConfigName}      = "config.xml";
