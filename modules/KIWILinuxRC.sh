a0a35e3809e507e40f391841583ea9083fc1f2dd
Marcus Schäfer <ms@suse.com>
2016-01-11 12:54:57 +0100

    Use mount options to increase overlay performace
    For overlay filesystems not writing into a tmpfs performance is
    more important than safety. We use this combination of options
    for now, if you encounter stability problems please let us know

--- modules/KIWILinuxRC.sh
+++ modules/KIWILinuxRC.sh
@@ -6526,6 +6526,7 @@ function mountSystemUnionFS {
     local roDevice=`echo $UNIONFS_CONFIG | cut -d , -f 2`
     local unionFST=`echo $UNIONFS_CONFIG | cut -d , -f 3`
     local prefix=/mnt
+    local mount_options
     #======================================
     # load fuse module
     #--------------------------------------
@@ -6572,7 +6573,13 @@ function mountSystemUnionFS {
         if [ ! "$roDevice" = "nfs" ] && ! setupReadWrite; then
             return 1
         fi
-        if ! kiwiMount "$rwDevice" "$rwDir";then
+        if [ "$kiwi_hybridpersistent" = "true" ];then
+            # When using an overlay writing to a block device safety has
+            # less priority over speed. If this does not match your use
+            # case please report an issue on the kiwi github
+            mount_options="-o defaults,async,relatime,nodiratime,barrier=1"
+        fi
+        if ! kiwiMount "$rwDevice" "$rwDir" "$mount_options";then
             Echo "Failed to mount read/write filesystem"
             return 1
         fi
