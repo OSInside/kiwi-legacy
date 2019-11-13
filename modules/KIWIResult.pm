OSInside/kiwi-legacy

commit 166cd36d0c791280b7ed2c5301a446c4322ae61f

Sort filesystem listings (#672)
so that kiwi works in a reproducible way
in spite of indeterministic filesystem readdir order.

See https://reproducible-builds.org/ for why this is good.

modules/KIWIResult.pm
@@ -486,7 +486,7 @@ sub __sign_with_sha256sum {
         $kiwi -> failed ();
         return;
     }
-    while (my $entry = readdir ($dh)) {
+    while (my $entry = sort(readdir ($dh))) {
         next if $entry eq "." || $entry eq "..";
         next if ! -f "$tmpdir/$entry";
         my $alg = 'sha256';

modules/KIWICollect.pm
@@ -2091,7 +2091,7 @@ sub collectProducts {
             $this->logMsg('I', "No products found, skipping");
             next RELEASEPACK;
         }
-        my @r = grep {$_ =~ '\.prod$'} readdir(D);
+        my @r = grep {$_ =~ '\.prod$'} sort(readdir(D));
         closedir D;
 
         # read each product file

modules/KIWIContainerBuilder.pm
@@ -352,7 +352,7 @@ sub __createContainerBundle {
     }
     my @dirlist;
     if (opendir my($dh), $origin) {
-        @dirlist = grep { !/^\.\.?$/x } readdir $dh;
+        @dirlist = grep { !/^\.\.?$/x } sort(readdir $dh);
         closedir $dh;
     } else {
         $kiwi -> failed();

modules/KIWIRepoMetaHandler.pm
@@ -114,7 +114,7 @@ sub loadPlugins {
         );
         return ($loaded, $avail);
     }
-    my @plugins = readdir(PLUGINDIR);
+    my @plugins = sort(readdir(PLUGINDIR));
     closedir(PLUGINDIR);
     my %plugins;
     foreach my $p(@plugins) {

modules/KIWIRoot.pm
@@ -1194,7 +1194,7 @@ sub setup {
         }
         $kiwi -> done();
         my @scriptList = readdir $FD;
-        foreach my $script (@scriptList) {
+        foreach my $script (sort(@scriptList)) {
             if (-f "$root/image/config/$script") {
                 if ($manager -> setupPackageInfo ( $script )) {
                     next;
@@ -1471,7 +1471,7 @@ sub importHostPackageKeys {
     }
     my @rpm_keys = ();
     if (opendir (my $FD,$sigs)) {
-        @rpm_keys = readdir ($FD); closedir ($FD);
+        @rpm_keys = sort(readdir ($FD)); closedir ($FD);
     }
     if (@rpm_keys <= 2) {
         $kiwi -> skipped ();
@@ -1518,7 +1518,7 @@ sub exportHostPackageKeys {
     }
     my @rpm_keys = ();
     if (opendir (my $FD,"$root/rpm-sigs")) {
-        @rpm_keys = readdir ($FD); closedir ($FD);
+        @rpm_keys = sort(readdir ($FD)); closedir ($FD);
     }
     if (@rpm_keys <= 2) {
         return $this;

modules/KIWIRuntimeChecker.pm
@@ -957,7 +957,7 @@ sub __checkRootDirOutsideImageDescription {
     my @forbidden = ('config.xml', '*.kiwi');
     my $looks_like_description_dir;
     if (opendir (my $FD, $parent_dir)) {
-        my @dir_entries = readdir ($FD);
+        my @dir_entries = sort(readdir ($FD));
         closedir ($FD);
         foreach my $entry (@dir_entries) {
             if ($entry eq 'config.xml' || $entry =~ /\.kiwi$/) {
@@ -1932,7 +1932,7 @@ sub __read_pids {
     my $path = shift;
     my @pids;
     if (opendir (my $FD, $path)) {
-        foreach my $pid (readdir $FD) {
+        foreach my $pid (sort(readdir $FD)) {
             next if $pid !~ /^\d+$/;
             push @pids, $pid;
         }

modules/KIWIUtil.pm
@@ -260,7 +260,7 @@ sub splitPathLocal {
     my $rest   = $testlist[1];
     # read current dir to list before descent:
     opendir(DIR, $basepath) or return;
-    my @dirlist = readdir(DIR);
+    my @dirlist = sort(readdir(DIR));
     closedir(DIR);
     $this->{m_collect}->logMsg("I", "Current local directory is $basepath");
     my $atleastonce = 0;

commit 29281527b6c8f60d9762eded21d8276b8ff5bda5

fix sha256 checksum file creation
This got broken in commit 166cd36d0c79 ("Sort filesystem listings...")
Also fixes "Useless use of sort in scalar context..." runtime warning.

modules/KIWIResult.pm
@@ -484,7 +484,7 @@ sub __sign_with_sha256sum {
         $kiwi -> failed ();
         return;
     }
-    while (my $entry = sort(readdir ($dh))) {
+    foreach my $entry (sort(readdir($dh))) {
         next if $entry eq "." || $entry eq "..";
         next if ! -f "$tmpdir/$entry";
         my $alg = 'sha256';